8921c3601ea92f04cf9c0c0396d13457aa733361
Gabrielbarbero <gabrielbarberog@gmail.com>
2025-10-29 00:38:17 -0300

    ultimo desafio em python

---
+++ Desafios/Desafio 31-32.py
@@ -0,0 +1,14 @@
+#desafio 31
+'''
+valor = int(input("coloque o numero: "))
+parimpar = lambda valor:"par" if valor%2 == 0  else "impar"
+
+print(parimpar(valor))
+'''
+
+#desafio 32
+
+potencia = lambda potencial,: potencial ** 2
+potencial = int(input("De o valor do numero"))
+for v in range (potencial,1,-1):
+    print(potencia(v))
